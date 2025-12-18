b644afb78634be3722ff5106ea699c1d963bbb56
R-A-Y-A-N <hamennirayanwork@gmail.com>
2025-12-18 23:45:43 +0100

    add images

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,7 @@ const Projects = () => {
       title: t("title3"),
       description: t( "description3"),
       technologies: ["React", "Next.js", "Tailwind CSS"],
-      image: "/images/k1.png",
+      image: "/images/ia.webp",
       
     },
     
